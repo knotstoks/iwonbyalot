801c9cd95a92aa8d38a5f9f38b1f8a1d2b2a65ad
Ger Hean <gerhean@live.com>
2021-01-06 01:39:31 +0800

    add dialogue classes

---
+++ Assets/Scripts/Classes/DialogueEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New DialogueEvent", menuName = "ScriptableObjects/DialogueEvent", order = 1)]
+public class DialogueEvent : ScriptableObject
+{
+    public enum DialogueLineType
+    {
+        // Normal Text
+        Text,
+        // Uses String Format.
+        Formated,
+        // Jump to the line at the specified index
+        Jump,
+        // Line to display when making choice
+        StartChoice,
+        Choice,
+        // Indicate that choice has been listed out
+        ChoiceEnd,
+        // Return with the string specified
+        Return,
+    }
+    public List<(DialogueLineType, string, int)> dialogueLines;
+}
--- Assets/Scripts/Description.cs
+++ Assets/Scripts/Description.cs
@@ -14,10 +14,57 @@ public class Description : MonoBehaviour
     private List<string> dialogueList;
     private int dialogueProgression;
 
+    private Coroutine speaking = null;
+    private string targetSpeech = "";
+    [HideInInspector] public bool isWaitingForUserInput = false;
+
     // Start is called before the first frame update
     void Start()
     {
 
+    }
+
+    /// <summary>
+    /// Say something and show it on the speech box.
+    /// </summary>
+    public void Say(string speech)
+    {
+        StopSpeaking();
+
+        speaking = StartCoroutine(Speaking(speech));
+    }
+
+    public void StopSpeaking()
+    {
+        if (isSpeaking)
+        {
+            StopCoroutine(speaking);
+        }
+        speaking = null;
+    }
+
+    public bool isSpeaking { get { return speaking != null; } }
+    
+    IEnumerator Speaking(string speech)
+    {
+        targetSpeech = speech;
+
+        textfield.text = "";
+
+        isWaitingForUserInput = false;
+
+        while (textfield.text != targetSpeech)
+        {
+            textfield.text += targetSpeech[textfield.text.Length];
+            yield return new WaitForEndOfFrame();
+        }
+
+        //text finished
+        isWaitingForUserInput = true;
+        while (isWaitingForUserInput)
+            yield return new WaitForEndOfFrame();
+
+        StopSpeaking();
     }
 
     public void executeDialogue(List<string> dialogueList, ResumeGameCallback resumeGameCallback)
@@ -31,7 +78,7 @@ public class Description : MonoBehaviour
         this.dialogueProgression = 1;
         this.dialogueList = dialogueList;
         this.resumeGameCallback = resumeGameCallback;
-        this.textfield.text = dialogueList[0];
+        Say(dialogueList[0]);
         gameObject.GetComponent<Button>().interactable = true;
     }
 
@@ -46,7 +93,7 @@ public class Description : MonoBehaviour
                 gameObject.GetComponent<Button>().interactable = false;
                 return;
             }
-            textfield.text = dialogueList[dialogueProgression];
+            Say(dialogueList[dialogueProgression]);
             dialogueProgression += 1;
         }
     }
