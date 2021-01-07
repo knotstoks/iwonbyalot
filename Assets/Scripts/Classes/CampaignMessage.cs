2862729bd615a447b5cddfa9f9fe0059256d1c30
Ger Hean <gerhean@live.com>
2021-01-07 18:54:50 +0800

    add CampaignMessage class

---
+++ Assets/Scripts/Classes/CampaignMessage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Campaign Message", menuName = "ScriptableObjects/CampaignMessage", order = 1)]
+public class CampaignMessage : ScriptableObject
+{
+    public string messageName;
+    public string speech;
+    public string like;
+    public string neutral;
+    public string dislike;
+}
